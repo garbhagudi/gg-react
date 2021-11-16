3a2971471fde0fafc6eed7b03029c6135cc4689f
garbhagudi <garbhagudi.11@gmail.com>
2021-11-16 18:35:28 +0530

    demo staging

---
+++ src/demos/FemaleInfertility.js
@@ -0,0 +1,12 @@
+import React from "react";
+import loadable from "@loadable/component";
+
+const FemaleInfertility = () => {
+  return (
+    <div>
+      <span>Female Infertility</span>
+    </div>
+  );
+};
+
+export default FemaleInfertility;
---
+++ src/demos/Paripoorna.js
@@ -0,0 +1,12 @@
+import React from "react";
+import loadable from "@loadable/component";
+
+const Paripoorna = () => {
+  return (
+    <div>
+      <span>Paripoorna</span>
+    </div>
+  );
+};
+
+export default Paripoorna;
